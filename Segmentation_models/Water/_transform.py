f58cb72f7d5fb2df641a8d3f5a096f4babc15911
dimakiss <40802862+dimakiss@users.noreply.github.com>
2023-08-20 14:23:53 +0300

    Update _transform.py

--- Segmentation_models/Water/_transform.py
+++ Segmentation_models/Water/_transform.py
@@ -3,45 +3,41 @@ import numpy as np
 from PIL import Image
 import albumentations as A
 
-def get_transforms(level='basic', dim=320):
-    common_transforms = [
+
+def get_transforms():
+    dim = 320
+
+    clahe_transform = A.CLAHE(p=1)  # CLAHE transformation
+    grid_distortion = A.GridDistortion(p=1)  # Grid Distortion transformation
+    optical_distortion = A.OpticalDistortion(p=1)  # Optical Distortion transformation
+    elastic_transform = A.ElasticTransform(p=0.5, alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03) # Elastic transformation
+    random_brightness_contrast = A.RandomBrightnessContrast(p=0.5) # Random brightness and contrast
+
+    def apply_custom_transforms(img):
+        #img_out = clahe_transform(image=np.array(img))["image"]
+        #img_out = grid_distortion(image=img_out)["image"]  # Apply Grid Distortion
+        #img_out = optical_distortion(image=img_out)["image"]  # Apply Optical Distortion
+        #img_out = elastic_transform(image=img_out)["image"]  # Apply Elastic Transformation
+        #img_out = random_brightness_contrast(image=img_out)["image"]  # Apply Random Brightness and Contrast
+        return img #Image.fromarray(img_out)
+
+    train_image_transform = transforms.Compose([
+        lambda img: apply_custom_transforms(img),
+        transforms.Resize((dim, dim)),
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.RandomVerticalFlip(p=0.5),
+        transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.1),
+        transforms.RandomRotation(degrees=15),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+
+    train_mask_transform = transforms.Compose([
+        lambda img: apply_custom_transforms(img), # Applying CLAHE to the mask
         transforms.Resize((dim, dim)),
         transforms.RandomHorizontalFlip(p=0.5),
         transforms.RandomVerticalFlip(p=0.5),
         transforms.RandomRotation(degrees=15),
-    ]
-
-    if level == 'medium':
-        common_transforms += [
-            transforms.RandomResizedCrop(dim),
-        ]
-
-    if level == 'extreme':
-        common_transforms += [
-            A.CLAHE(p=1),
-            A.GridDistortion(p=1),
-            A.OpticalDistortion(p=1),
-            A.ElasticTransform(p=0.5, alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03),
-            A.RandomBrightnessContrast(p=0.5),
-            A.GaussianBlur(p=0.5),
-            A.RandomCrop(height=dim, width=dim, p=0.5),
-            A.RandomScale(scale_limit=0.2, p=0.5),
-        ]
-
-    def apply_transforms(img, mask=None):
-        augmented = A.Compose(common_transforms)(image=np.array(img), mask=np.array(mask))
-        return Image.fromarray(augmented["image"]), Image.fromarray(augmented["mask"])
-
-    
-    image_transforms = transforms.Compose([
-        lambda img, mask: apply_transforms(img, mask),
-        transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.1),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ])
-    
-    mask_transforms = transforms.Compose([
-        lambda img, mask: apply_transforms(img, mask),
         transforms.ToTensor(),
     ])
 
@@ -56,84 +52,4 @@ def get_transforms(level='basic', dim=320):
         transforms.ToTensor(),
     ])
 
-    return image_transforms, mask_transforms, val_image_transform, val_mask_transform
-
-
-
-
-
-
-
-
-
-
-
-#from torchvision import transforms
-#from scipy import ndimage
-#import numpy as np
-#from PIL import Image
-#import albumentations as A
-#import cv2
-#
-#def get_transforms():
-#    dim = 320
-#
-#    clahe_transform = A.CLAHE(p=1)
-#    grid_distortion = A.GridDistortion(p=1)
-#    optical_distortion = A.OpticalDistortion(p=1)
-#    elastic_transform = A.ElasticTransform(p=0.5, alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03)
-#    random_brightness_contrast = A.RandomBrightnessContrast(p=0.5)
-#    gaussian_blur = A.GaussianBlur(p=0.5)
-#    random_crop = A.RandomCrop(height=dim, width=dim, p=0.5)
-#    random_resize = A.RandomScale(scale_limit=0.2, p=0.5)
-#
-#    def apply_custom_transforms(img, mask=None):
-#        augmented = A.Compose([
-#            clahe_transform,
-#            grid_distortion,
-#            optical_distortion,
-#            elastic_transform,
-#            random_brightness_contrast,
-#            gaussian_blur,
-#            random_crop,
-#            random_resize
-#        ])(image=np.array(img), mask=np.array(mask))
-#
-#        return Image.fromarray(augmented["image"]), Image.fromarray(augmented["mask"])
-#
-#    train_transforms = transforms.Compose([
-#        lambda img, mask: apply_custom_transforms(img, mask),
-#        transforms.RandomHorizontalFlip(p=0.5),
-#        transforms.RandomVerticalFlip(p=0.5),
-#        transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.1),
-#        transforms.RandomRotation(degrees=15),
-#        transforms.RandomAffine(degrees=15, translate=(0.1, 0.1), shear=10),
-#        transforms.Resize((dim, dim)),
-#        transforms.ToTensor(),
-#        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-#    ])
-#
-#    mask_transforms = transforms.Compose([
-#        transforms.RandomHorizontalFlip(p=0.5),
-#        transforms.RandomVerticalFlip(p=0.5),
-#        transforms.RandomRotation(degrees=15),
-#        transforms.RandomAffine(degrees=15, translate=(0.1, 0.1), shear=10),
-#        transforms.Resize((dim, dim)),
-#        transforms.ToTensor(),
-#    ])
-#
-#    val_image_transform = transforms.Compose([
-#        transforms.Resize((dim, dim)),
-#        transforms.ToTensor(),
-#        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-#    ])
-#
-#    val_mask_transform = transforms.Compose([
-#        transforms.Resize((dim, dim)),
-#        transforms.ToTensor(),
-#    ])
-#
-#    return train_transforms, mask_transforms, val_image_transform, val_mask_transform
-#
-
-
+    return train_image_transform, train_mask_transform, val_image_transform, val_mask_transform
