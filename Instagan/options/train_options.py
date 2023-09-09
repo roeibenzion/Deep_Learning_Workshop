c531003fd02d945d180fc930a91918febfc33a38
dimakiss <40802862+dimakiss@users.noreply.github.com>
2023-09-09 15:37:01 +0300

    Update train_options.py

--- Instagan/options/train_options.py
+++ Instagan/options/train_options.py
@@ -28,6 +28,7 @@ class TrainOptions(BaseOptions):
         parser.add_argument('--no_html', action='store_true', help='do not save intermediate training results to [opt.checkpoints_dir]/[opt.name]/web/')
         parser.add_argument('--lr_policy', type=str, default='lambda', help='learning rate policy: lambda|step|plateau|cosine')
         parser.add_argument('--lr_decay_iters', type=int, default=50, help='multiply by a gamma every lr_decay_iters iterations')
+        parser.add_argument('--more_aug', action='store_true', help='more aug')
 
         self.isTrain = True
         return parser
